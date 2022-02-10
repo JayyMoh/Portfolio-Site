335a5a0ad3a55ba3e1604fee061817dc31ed35bb
Jonathan <jonmohr@outlook.com>
2022-02-10 11:39:53 -0500

    Mobile Navbar and responsiveness fixed

--- public/asset/portfolio.js
+++ public/asset/portfolio.js
@@ -1,1 +1,10 @@
 console.log('JS is connected')
+
+// closes the mobile menu after clicking anchor link
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarNavAltMarkup')
+const bsCollapse = new bootstrap.Collapse(menuToggle, ({toggle: false}))
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
+
